dc2b3715321405f21e38826a4d8c6b3605f2ab5d
vipinvsist <vashistvipin42@gmail.com>
2025-10-03 23:30:30 +0530

    memory

---
+++ first.py
@@ -0,0 +1,19 @@
+import os
+import getpass
+from dotenv import load_dotenv
+load_dotenv()
+if "PPLX_API_KEY" not in os.environ:
+    os.environ['PPLX_API_KEY']=getpass.getpass("Enter your PPLX API:\n ")
+
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_perplexity import ChatPerplexity
+
+chat = ChatPerplexity(temperature=0, model="sonar-pro")
+system = "You are a helpful assistant."
+human = "{input}"
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+chain = prompt | chat
+response = chain.invoke({"input": "Why is the Higgs Boson important?"})
+print(response.content)
